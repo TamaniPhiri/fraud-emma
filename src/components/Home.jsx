e9382d662cb629feb51bb0ee2bafb37e08946f34
tamani phiri <tamanigabriel0@gmail.com>
2023-10-27 08:08:21 +0200

    c

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,8 +18,20 @@ const Home = () => {
             <span className="text-gray-600">{user.email}</span>
           </div>
         </div>
-        <div className=" text-center w-full my-6">
+        <div className=" text-center w-full my-6 gap-4 flex items-center justify-center flex-col">
           <h1 className=" text-5xl font-bold">Fraud detection system</h1>
+          <p className=" lg:max-w-2xl md:max-w-lg w-full">
+            Welcome to the future of safeguarding your financial world with our
+            cutting-edge fraud detection web app. In an ever-evolving digital
+            landscape, security is paramount. Our solution is your vigilant
+            partner, tirelessly monitoring every transaction to ensure your
+            peace of mind. With state-of-the-art machine learning algorithms and
+            real-time alerts, we stay one step ahead of the fraudsters, so you
+            can continue to thrive and prosper, knowing your assets are
+            protected by the industry&#39;s best. Say goodbye to sleepless
+            nights and hello to a brighter, more secure financial future. Join
+            us today in the fight against financial fraud!
+          </p>
         </div>
         <BarChart />
         <div>fl</div>
