d4c9f504ce66310a460bdbf528826cebc17d53e2
tamani phiri <tamanigabriel0@gmail.com>
2023-10-31 13:13:56 +0200

    c

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Home from "./components/Home";
 import History from "./components/History";
 import Transactions from "./components/Transactions";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/history" element={<History />} />
-          <Route path="/transactions" element={<Transactions />} />
-          <Route path="/" Component={Login}/>
-        </Routes>
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/history" element={<History />} />
+      <Route path="/transactions" element={<Transactions />} />
+      <Route path="/" Component={Login} />
+      <Route path="/register" Component={Register} />
+    </Routes>
   );
 };
 
---
+++ src/pages/Register.jsx
@@ -0,0 +1,126 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useState } from "react";
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userName, setUserName] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [loading, setloading] = useState(false);
+  const [error, setError] = useState("");
+  const userLogin = async (e) => {
+    if (
+      userName.trim() === "" ||
+      password.trim() === "" ||
+      userName.trim() === "" ||
+      fullName.trim() === "" ||
+      phoneNumber.trim() === ""
+    ) {
+      return alert("Please fill in the missing fields");
+    }
+    setError("");
+    try {
+      setloading(true);
+      e.preventDefault();
+      const response = await axios.post("https://dummyjson.com/auth/login", {
+        username: userName,
+        password: password,
+        full_name: fullName,
+        email: email,
+        phone_number: phoneNumber,
+      });
+      if (response.status === 200) {
+        console.log(response.data);
+        Cookies.set("user", JSON.stringify(response.data), {
+          sameSite: "None",
+        });
+        window.location.href = "/home";
+      }
+    } catch (error) {
+      console.log(error);
+      setError("Authentication failed");
+    } finally {
+      setloading(false);
+    }
+  };
+  return (
+    <div className=" w-full grid lg:grid-cols-2 grid-cols-1 items-center justify-center min-h-screen">
+      <div className="bg-blue-800 text-white h-full w-full flex items-center justify-center">
+        <div className="grid gap-2 w-full max-w-full md:max-w-xs px-4">
+          <span className=" font-bold">Username</span>
+          <input
+            onChange={(e) => setUserName(e.target.value)}
+            type="text"
+            name=""
+            id=""
+            className=" text-black focus:outline-none p-2 rounded-md w-full"
+          />
+
+          <span className=" font-bold">Phone Number</span>
+          <input
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            type="text"
+            name=""
+            id=""
+            className=" text-black focus:outline-none p-2 rounded-md w-full"
+          />
+          <span className=" font-bold">Full name</span>
+          <input
+            onChange={(e) => setFullName(e.target.value)}
+            type="text"
+            name=""
+            id=""
+            className=" text-black focus:outline-none p-2 rounded-md w-full"
+          />
+          <span className=" font-bold">Phone Number</span>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            name=""
+            id=""
+            className=" text-black focus:outline-none p-2 rounded-md w-full"
+          />
+
+          <span className=" font-bold">Password</span>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            name=""
+            id=""
+            className=" text-black focus:outline-none p-2 rounded-md w-full"
+          />
+
+          {loading ? (
+            <button
+              disabled
+              onClick={userLogin}
+              className=" w-full bg-blue-900 border font-semibold flex items-center justify-center border-gray-500 active:scale-95 duration-200 p-2 rounded-md mt-4"
+            >
+              <div className="w-7 h-7 border-2 border-t-blue-900 animate-spin rounded-full"></div>
+            </button>
+          ) : (
+            <button
+              onClick={userLogin}
+              className=" w-full bg-blue-900 border font-semibold border-gray-500 active:scale-95 duration-200 p-2 rounded-md mt-4"
+            >
+              Login
+            </button>
+          )}
+          <span>{error}</span>
+        </div>
+      </div>
+      <div className="lg:flex hidden w-full">
+        <img
+          src="/src/assets/bgemma.jpg"
+          alt="bg Image"
+          loading="lazy"
+          className=" object-cover w-full h-screen"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Register;
